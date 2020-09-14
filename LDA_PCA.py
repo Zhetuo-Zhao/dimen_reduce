8dea463bdcd1c5560423168949e7a7131b365e4c
zzhaoaa <zzhaoaa@gmail.com>
2020-09-14 13:53:22 -0400

    add other methods of dimension reduction

--- LDA_PCA.py
+++ LDA_PCA.py
@@ -3,8 +3,10 @@ import data_plot
 import matplotlib.figure as fig
 import matplotlib.pyplot as plt
 from sklearn import datasets
-from sklearn.decomposition import PCA
+from sklearn.decomposition import FastICA, PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn import manifold
+from time import time
 import pdb
 
 digits=datasets.load_digits(n_class=6)
@@ -12,20 +14,28 @@ digits=datasets.load_digits(n_class=6)
 X=digits.data
 y=digits.target
 n_samples, n_features = X.shape
-
+n_comp=2
 
 Methods={
-    'PCA' : PCA(n_components=2),
-    'LDA' : LinearDiscriminantAnalysis(n_components=2),
+    'PCA' : PCA(n_components=n_comp),
+    'LDA' : LinearDiscriminantAnalysis(n_components=n_comp),
+    'MDS' : manifold.MDS(n_components=n_comp, max_iter=200, n_init=1),
+    'tSNE' : manifold.TSNE(n_components=n_comp, init='pca', random_state=0),
+    'ICA' : FastICA(n_components=n_comp,random_state=0)
     }
-X_r=Methods['PCA'].fit(X).transform(X)
-plt.figure()
-data_plot.plot_digits_embedding(X_r,y)
 
 
-X_r2=Methods['LDA'].fit(X,y).transform(X)
-plt.figure()
-data_plot.plot_digits_embedding(X_r2,y)
 
-print('explained variance ratio, PCA:',Methods['PCA'].explained_variance_ratio_,'lDA: ',Methods['LDA'].explained_variance_ratio_)
+for i, (label,method) in enumerate(Methods.items()):
+    t0=time()
+    if label=='LDA':
+        Y=method.fit_transform(X,y)
+    else:      
+        Y=method.fit_transform(X)
+    t1=time()
+    
+    print("%s: %.2g sec" % (label, t1 - t0))
 
+    #pdb.set_trace()
+    data_plot.plot_digits_embedding(Y, y,label)
+    
